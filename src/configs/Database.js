e247f7d67d8290b3e6bfc57df18c4a4e5eff5c70
RandaAlfizah <randaalfiizh@gmail.com>
2024-06-27 10:04:07 +0700

    udah saya connect ke database guys, ntar kalian tinggal lanjut coding aja

--- src/configs/Database.js
+++ src/configs/Database.js
@@ -3,9 +3,10 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    '',
     {
         host: process.env.DB_HOST,
+        port: 3307,
         dialect: process.env.DB_DIALECT,
         dialectOptions: {
             connectTimeout: 30000,
